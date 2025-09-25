ea01b18498ecf949aafce350d43041d95668bb44
Esther Wandera <wanderaesther809@gmail.com>
2025-09-25 16:23:47 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// remove the <main> with id 'main'
+document.querySelector("main#main").remove();
+
+// create a new h1 element
+const newHeader = document.createElement("h1");
+
+// set its id to 'victory'
+newHeader.id = "victory";
+
+// set its text content to include your name
+newHeader.textContent = "Esther is the champion"; // replace Esther with your name
